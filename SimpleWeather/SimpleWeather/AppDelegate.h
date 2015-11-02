9d1df7f1a2b98dfa5bc624e7b41417fddd3c58e7
hanxu <1547213@qq.com>
2015-11-02 15:42:27 +0800

    init

---
+++ SimpleWeather/SimpleWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by hanxu on 15/11/2.
+//  Copyright © 2015年 __zimu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleWeather/SimpleWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWeather
+//
+//  Created by hanxu on 15/11/2.
+//  Copyright © 2015年 __zimu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
