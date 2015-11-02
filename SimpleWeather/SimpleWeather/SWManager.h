1ea5888564a8a1d05ed5383b39896301530a240b
hanxu <1547213@qq.com>
2015-11-02 17:02:58 +0800

    mantle

---
+++ SimpleWeather/SimpleWeather/SWManager.h
@@ -0,0 +1,13 @@
+//
+//  SWManager.h
+//  SimpleWeather
+//
+//  Created by hanxu on 15/11/2.
+//  Copyright © 2015年 __zimu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWManager : NSObject
+
+@end
---
+++ SimpleWeather/SimpleWeather/SWManager.m
@@ -0,0 +1,13 @@
+//
+//  SWManager.m
+//  SimpleWeather
+//
+//  Created by hanxu on 15/11/2.
+//  Copyright © 2015年 __zimu. All rights reserved.
+//
+
+#import "SWManager.h"
+
+@implementation SWManager
+
+@end
