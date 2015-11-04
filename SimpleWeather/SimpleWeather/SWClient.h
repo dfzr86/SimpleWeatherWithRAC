4446d76e8526aadfb7121ca92df8765863db3628
hanxu <1547213@qq.com>
2015-11-04 12:40:05 +0800

    container

---
+++ SimpleWeather/SimpleWeather/SWClient.h
@@ -0,0 +1,13 @@
+//
+//  SWClient.h
+//  SimpleWeather
+//
+//  Created by hanxu on 15/11/3.
+//  Copyright © 2015年 __zimu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWClient : NSObject
+
+@end
---
+++ SimpleWeather/SimpleWeather/SWClient.m
@@ -0,0 +1,13 @@
+//
+//  SWClient.m
+//  SimpleWeather
+//
+//  Created by hanxu on 15/11/3.
+//  Copyright © 2015年 __zimu. All rights reserved.
+//
+
+#import "SWClient.h"
+
+@implementation SWClient
+
+@end
